f42651f34f9465990c28eb2debb9f203a1d6960a
Konstantinos Filios <kfilios@novibet.com>
2025-08-12 15:07:47 +0300

    create send message with result not only fire and forget

--- src/main/kotlin/com/crp/system/libs/kafka/KafkaMessageProducer.kt
+++ src/main/kotlin/com/crp/system/libs/kafka/KafkaMessageProducer.kt
@@ -3,6 +3,8 @@ package com.crp.system.libs.kafka
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult
+import java.util.concurrent.CompletableFuture
 
 @Component
 data class KafkaMessageProducer(
@@ -11,5 +13,9 @@ data class KafkaMessageProducer(
 
     fun sendMessage(topic: String, message: String) {
         kafkaTemplate.send(topic, message)
+    }
+
+    fun sendMessageWithResult(topic: String, message: String): CompletableFuture<SendResult<String?, String?>?> {
+        return kafkaTemplate.send(topic, message)
     }
 }
